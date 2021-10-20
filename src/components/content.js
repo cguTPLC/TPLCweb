320a0c96ebf397e1822257816c6a8e39d8f8ea22
Dennis <dennis1989999@gmail.com>
2021-10-21 00:10:33 +0800

    datalinker

--- src/components/content.js
+++ src/components/content.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import {Route} from 'react-router-dom'
-
+import Home from './pages/Home.js';
+import About from './pages/About.js';
+import Users from './pages/Users.js';
 
 export default function Content (){
+    
     return (
         <div className="content">
-            <Route path="/TPLCweb" exact component={Index} />
+            <Route path="/TPLCweb" exact component={Home} />
             <Route path="/TPLCweb/about" component={About} />
             <Route path="/TPLCweb/users" component={Users} />
             <br/>
@@ -44,9 +47,3 @@ export default function Content (){
         </div>
     )
 }
-
-
-
-const Index = () => <h2>Home</h2>
-const About = () => <h2>About</h2>
-const Users = () => <h2>Users</h2>
---
+++ src/components/pages/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Home = () => {
+    return(
+    <>
+        {data.map(data => 
+        <div>
+            {data.name}他{data.age}歲 廉價大學生做網頁 <b>error404</b>
+            <b>error404</b>
+            <b>error404</b>
+            <b>error404</b>
+            <b>error404</b>
+            <b>error404</b>
+            <b>error404</b>
+        </div>
+        )}
+    </>
+    )
+}
+export default Home;
+
+const data = [
+    {
+        name: "翁旻醇",
+        age: "20"
+    },
+]
